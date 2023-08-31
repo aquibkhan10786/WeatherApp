936c4469c7b536740fa1d089056981d327d4a922
aquibkhan10786 <aquibkhan10786@gmail.com>
2023-08-31 14:29:50 +0530

    Update WeatherCard.js

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import '../App.css';
 const kelvinToCelsius = (kelvin) => kelvin - 273.15;
+  const today=new Date();
+  const month = today.getMonth()+1;
+const year = today.getFullYear();
+const date = today. getDate();
+const currentDate =date + "/" + month  + "/" + year;
+const currTime = new Date().toLocaleTimeString();
 const WeatherCard = ({ weatherData }) => {
   const temperatureInCelsius = kelvinToCelsius(weatherData.main.temp);
   return (
     <div className="weather-card">
+       <p>Date:{currentDate}----Time:{currTime}</p>  
       <h2>{weatherData.name} ,{weatherData.sys.country}</h2>
 
       <p>Temperature:{temperatureInCelsius.toFixed(1)}°C</p>
